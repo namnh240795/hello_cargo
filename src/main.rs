96b015c5cebb9b736df5d109d53ef4b12acd376a
Nam Nguyen <nam.nh@agiletech.vn>
2022-02-06 20:44:55 +0700

    Check len

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
 fn main() {
-    println!("Hello, world!");
+    let x = 5;
+
+    let x = x + 1;
+
+    {
+        let x = x * 2;
+        println!("The value of x in the inner scope is: {}", x);
+    }
+
+    println!("The value of x is: {}", x);
+
+    let spaces = "     ";
+
+    println!("spaces: {}", spaces.len());
 }
